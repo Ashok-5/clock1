d78bb9b8bb0fddb11a570037c735aff1a7f5407d
Ashok-5 <112316102+Ashok-5@users.noreply.github.com>
2023-03-07 17:37:33 +0000

    just a litte push

---
+++ clk.js
@@ -0,0 +1,9 @@
+const secNdl=document.querySelector(".sec");
+const minNdl=document.querySelector(".min");
+const hrsNdl=document.querySelector(".hrs");
+function rotate(){
+    const date = new Date();
+    const secs = date.getSeconds();
+    const secdeg = ((secs / 60) * 360)+90;
+    secNdl.setAttribute("style","transform:rotate((secdeg)deg);")
+}
