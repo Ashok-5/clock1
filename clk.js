15a0f7f28076cad08be11bdaeaeae6a59a2a374a
Ashok-5 <112316102+Ashok-5@users.noreply.github.com>
2023-03-08 10:32:41 +0000

    halfway

--- clk.js
+++ clk.js
@@ -1,8 +1,20 @@
-const secNdl=document.querySelector(".sec");
-const minNdl=document.querySelector(".min");
-const hrsNdl=document.querySelector(".hrs");
-function rotate(){
+const secNdl=document.querySelector('.sec');
+const minNdl=document.querySelector('.min');
+const hrsNdl=document.querySelector('.hrs');
+
+function rotation(){
     const date = new Date();
     const secs = date.getSeconds();
     const secdeg = ((secs / 60) * 360) + 90;
+    secNdl.style.transform=`rotate(${secdeg}deg)`
+
+    const mins = date.getMinutes();
+    const mindeg = ((mins / 60) * 360) + ((secs/60)*6) + 90;
+    minNdl.style.transform=`rotate(${mindeg}deg)`
+
+    const hour = date.getHours();
+    const hourdeg = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hrsNdl.style.transform=`rotate(${hourdeg}deg)`
 }
+setInterval(rotation,1000);
+rotation();
