d5b7999356121cbf3f610edb997d664ca03f7c9e
Ashok-5 <112316102+Ashok-5@users.noreply.github.com>
2023-03-26 05:28:07 +0000

    done

--- clk.js
+++ clk.js
@@ -1,32 +1,45 @@
 const secNdl=document.querySelector('.sec');
 const minNdl=document.querySelector('.min');
 const hrsNdl=document.querySelector('.hrs');
-const date = new Date();
-const secs = date.getSeconds();
-const mins = date.getMinutes();
-const hour = date.getHours();
-
 function rotation(){
+    const date = new Date();
+    const secs = date.getSeconds();
     const secdeg = ((secs / 60) * 360) + 90;
     secNdl.style.transform=`rotate(${secdeg}deg)`
-
+    const mins = date.getMinutes();
     const mindeg = ((mins / 60) * 360) + ((secs/60)*6) + 90;
     minNdl.style.transform=`rotate(${mindeg}deg)`
-
+    const hour = date.getHours();
     const hourdeg = ((hour / 12) * 360) + ((mins/60)*30) + 90;
     hrsNdl.style.transform=`rotate(${hourdeg}deg)`
 }
 setInterval(rotation,1000);
 rotation();
+
 function updateClock() {
-    hour = (hour < 10 ? "0" : "") + hour;
-    mins = (mins < 10 ? "0" : "") + mins;
-    secs = (secs < 10 ? "0" : "") + secs;
-  
-    let time = hour + ":" + mins + ":" + secs;
-  
-    document.querySelector("dg_clk").innerHTML = time;
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  // Format hours, minutes, and seconds to ensure they
+  // display two digits.
+  if (hours < 10) {
+    hours = "0" + hours;
   }
-  
-  updateClock();
-  setInterval(updateClock, 1000);
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  // Build the clock as a string, e.g. "hh:mm:ss".
+  var timeString = hours + ":" + minutes + ":" + seconds;
+
+  // Update the clock's display.
+  document.querySelector(".dg_clk").innerHTML = timeString;
+}
+
+// Update the clock every second.
+setInterval(updateClock, 1000);
